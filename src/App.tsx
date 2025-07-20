297d77792bb33a3cca1c96e384690d6bd054e172
GPadaka19 <gustipadaka19@gmail.com>
2025-07-20 18:41:18 +0700

    fix: update background image source in App component for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/merch-wheel" element={<MerchWheel />} />
       </Routes>
       <div className="relative bg-stone-900 text-stone-100 min-h-screen">
-        <div className="absolute inset-0 bg-[url('/background/stone.webp')] opacity-10 bg-fixed"></div>
+        <div className="absolute inset-0 bg-[url('/background/Wallpaper_Landscape.webp')] opacity-10 bg-fixed"></div>
         <Navbar />
         <main className="relative">
           <HeroSection />
