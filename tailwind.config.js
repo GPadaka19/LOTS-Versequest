0ac2193ba2840b77243e52441b7a37dff8d779a2
GPadaka19 <gustipadaka19@gmail.com>
2025-07-22 21:29:18 +0700

    feat: add custom color palette to Tailwind configuration for enhanced theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: '#2C201B',
+        primary: '#F5F1E9',
+        secondary: '#44362E',
+        accent: '#EAA91D',
+        extraAccent: '#2B3322',
+      },
+    },
   },
   plugins: [],
 };
