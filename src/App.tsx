ef01b2f8e102644bc6f70b10a989c25fc2d698bb
GPadaka19 <gustipadaka19@gmail.com>
2025-07-23 01:59:25 +0700

    feat: replace GallerySection with CharacterGallery and MapGallery for enhanced content display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
-import GallerySection from './components/GallerySection';
+import CharacterGallery from './components/CharacterGallery';
+import MapGallery from './components/MapGallery';
 import FeaturesSection from './components/FeaturesSection';
 import DeveloperSection from './components/DeveloperSection';
 import DownloadSection from './components/DownloadSection';
@@ -9,12 +10,14 @@ import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import MerchWheel from './components/MerchWheel';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ColorGuide from './components/ColorGuide';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/merch-wheel" element={<MerchWheel />} />
+        <Route path="/color" element={<ColorGuide />} />
       </Routes>
       <div className="relative bg-stone-900 text-stone-100 min-h-screen">
         <div className="absolute inset-0 bg-[url('/background/Wallpaper_Landscape.webp')] opacity-10 bg-fixed"></div>
@@ -23,7 +26,8 @@ function App() {
           <HeroSection />
           <div className="relative z-10 bg-stone-900">
             <AboutSection />
-            <GallerySection />
+            <CharacterGallery />
+            <MapGallery />
             <FeaturesSection />
             <DeveloperSection />
             <DownloadSection />
