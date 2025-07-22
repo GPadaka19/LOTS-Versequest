9c1e2d018190f7028e8f7eb579f56c261e032841
GPadaka19 <gustipadaka19@gmail.com>
2025-07-23 02:00:57 +0700

    style: update Footer background and layout for improved aesthetics and visual depth

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
-// import React from 'react';
-
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
   return (
-    <footer className="py-8 bg-stone-900 text-stone-400 text-center">
-      <div className="container mx-auto px-4 flex flex-col items-center">
+    <footer className="py-8 relative bg-stone-900 text-stone-400 text-center">
+      <div className="absolute inset-0 bg-[url('/background/Wallpaper_Landscape.webp')] bg-cover bg-center opacity-5 bg-fixed pointer-events-none"></div>
+      <div className="container mx-auto px-4 flex flex-col items-center relative z-10">
         <img
           src="/icons/Logo_Name.webp"
           alt="Versequest Logo"
